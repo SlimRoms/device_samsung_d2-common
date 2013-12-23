5796e35958eab414f124167300be75835beab873
Dave Daynard <nardholio@gmail.com>
2013-12-22 22:59:31 -0500

    d2-common: camera: Add flag to disable face detection
    Change-Id: I5b07c46b75890e929da57fb16e293b9a95b3e177

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ TARGET_NEED_DISABLE_AUTOFOCUS := true
 ifeq ($(filter apexqtmo expressatt,$(VARIENT_MODEL)),)
   TARGET_NEED_CAMERA_ZSL := true
 endif
+TARGET_NEED_DISABLE_FACE_DETECTION := true
 
 # Needed for blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
